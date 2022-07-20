89994c150dffd625479554e2c263b8c0097258ee
Romain Cambonie <romain.cambonie@gmail.com>
2022-07-20 14:51:19 +0200

    v0.0.1

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { createManagedHttpClient } from "./ports/httpClient";
+export { HttpClientError } from "./errors/HttpClientError.error";
+export { HttpServerError } from "./errors/HttpServerError.error";
+export { ConnectionRefusedError } from "./errors/ConnectionRefused.error";
