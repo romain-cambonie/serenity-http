2947acf1b10c2a371a41125b3f52d39c00395702
Romain Cambonie <romain.cambonie@gmail.com>
2022-07-20 15:26:10 +0200

    feat: initial commit
    * v0.0.1
    
    * test release

--- src/errors/InfrastructureError.error.ts
+++ src/errors/InfrastructureError.error.ts
@@ -1,7 +1,10 @@
+type InfrastructureErrorCodes = "ECONNREFUSED" | "ECONNRESET";
+
 export class InfrastructureError extends Error {
   constructor(
     public override readonly message: string = "Infrastructure error",
-    public override readonly cause?: Error,
+    public override readonly cause: Error,
+    public readonly code: InfrastructureErrorCodes,
   ) {
     super();
     Error.captureStackTrace(this, this.constructor);
@@ -13,21 +16,22 @@ export class InfrastructureError extends Error {
 export class ConnectionRefusedError extends InfrastructureError {
   constructor(
     public override readonly message: string = "Could not connect to server",
-    public override readonly cause?: Error,
+    public override readonly cause: Error,
   ) {
-    super();
+    super(message, cause, "ECONNREFUSED");
     Error.captureStackTrace(this, this.constructor);
     this.name = this.constructor.name;
     this.message = message;
   }
 }
 
-export class ConfigurationError extends InfrastructureError {
+// TODO On veut probablement réessayer l'appel quand ça arrive
+export class ConnectionResetError extends InfrastructureError {
   constructor(
-    public override readonly message: string = "Invalid configuration",
-    public override readonly cause?: Error,
+    public override readonly message: string = "The other side of the TCP conversation abruptly closed its end of the connection",
+    public override readonly cause: Error,
   ) {
-    super();
+    super(message, cause, "ECONNRESET");
     Error.captureStackTrace(this, this.constructor);
     this.name = this.constructor.name;
     this.message = message;
@@ -36,3 +40,6 @@ export class ConfigurationError extends InfrastructureError {
 
 export const isConnectionRefusedError = (error: unknown): boolean =>
   (error as unknown as { code: string }).code === "ECONNREFUSED";
+
+export const isConnectionResetError = (error: unknown): boolean =>
+  (error as unknown as { code: string }).code === "ECONNRESET";
