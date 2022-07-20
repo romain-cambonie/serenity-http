b1d30673f8829675c0517a96899ce929ba7a4765
Romain Cambonie <romain.cambonie@gmail.com>
2022-07-20 14:57:38 +0200

    test release

--- index.ts
+++ index.ts
@@ -1,4 +1,1 @@
-export { createManagedHttpClient } from "./ports/httpClient";
-export { HttpClientError } from "./errors/HttpClientError.error";
-export { HttpServerError } from "./errors/HttpServerError.error";
-export { ConnectionRefusedError } from "./errors/ConnectionRefused.error";
+export * from 'src/httpClient'
